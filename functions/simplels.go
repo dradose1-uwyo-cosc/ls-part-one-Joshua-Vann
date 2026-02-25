cc77f37720d918ec4d33e13d15785767b542f394
Joshua-Vann <jvann2@uwyo.edu>
2026-02-25 10:06:20 -0700

    Update simplels.go

--- functions/simplels.go
+++ functions/simplels.go
@@ -16,7 +16,7 @@ type color struct {
 
 // A simple ls for when no flags provided
 func SimpleLS(w io.Writer, args []string, useColor bool) {
-	blue := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
+	blue := color{start: []byte("\x1b[34;2;255;196;37m"), end: []byte("\x1b[0m")}
 	green := color{start: []byte("\x1b[32m"), end: []byte("\x1b[0m")}
 	//def := color{start: []byte("\x1b[37m"), end: []byte("\x1b[0m")}
 	for _, a := range args {
