8032c33a7706488b15ee52df18a1ef2ea1512068
Joshua-Vann <jvann2@uwyo.edu>
2026-02-24 20:26:46 -0700

    Update simplels.go

--- functions/simplels.go
+++ functions/simplels.go
@@ -4,9 +4,33 @@
 
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-//A simple ls for when no flags provided
+type color struct {
+	start []byte
+	end   []byte
+}
+
+// A simple ls for when no flags provided
 func SimpleLS(w io.Writer, args []string, useColor bool) {
-	return
+	//blue := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
+	//green := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
+	def := color{start: []byte("\x1b[37m"), end: []byte("\x1b[0m")}
+	for _, a := range args {
+		if !useColor {
+			def.ColorPrint(w, a)
+		} else {
+			fi, err := os.Lstat(a)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error with file")
+				return
+			} else {
+				print(fi)
+			}
+		}
+	}
 }
