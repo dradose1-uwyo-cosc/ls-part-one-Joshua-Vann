a4c8add027910525758427acca330c6b4dacc843
Joshua-Vann <jvann2@uwyo.edu>
2026-02-24 21:50:20 -0700

    Update simplels.go

--- functions/simplels.go
+++ functions/simplels.go
@@ -16,16 +16,21 @@ type color struct {
 
 // A simple ls for when no flags provided
 func SimpleLS(w io.Writer, args []string, useColor bool) {
-	//blue := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
-	//green := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
+	blue := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
+	green := color{start: []byte("\x1b[34m"), end: []byte("\x1b[0m")}
 	//def := color{start: []byte("\x1b[37m"), end: []byte("\x1b[0m")}
 	for _, a := range args {
 		if !useColor {
 			w.Write([]byte(a))
 		} else {
 			fi, _ := os.Lstat(a)
-			print(fi)
+			if fi.Mode().IsDir() {
+				blue.ColorPrint(w, a)
+			} else if fi.Mode().IsRegular() && (fi.Mode()&0111) != 0 {
+				green.ColorPrint(w, a)
+			} else {
+				w.Write([]byte(a))
+			}
 		}
 	}
 }
-
